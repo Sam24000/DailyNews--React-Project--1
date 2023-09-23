3be3e3217a7f6fe11db20a6fa737f57df34c0c45
samkumar <ksam65@gmail.com>
2023-09-23 19:17:15 +0530

    bug fixes

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -1,62 +1,55 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+// require('dotenv').config();
 
 const FetchApi = ({ cat }) => {
-    const [news, setnews] = useState();
+    const [news, setnews] = useState([]);
+    const [categ, setcateg] = useState(cat);
 
-    const fetch = async () => {
-        await axios.get(
-            cat ? `
-            https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=8362053158e04193b1c2eb350ce4df85`
-                :
-                "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8362053158e04193b1c2eb350ce4df85").then((res) =>
-
-
-                    setnews(res.data.articles),
-
-                )
-            .catch((err) =>
-                console.log(err))
-    }
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(
+                cat
+                    ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=${process.env.REACT_APP_kEY}`
+                    : `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${process.env.REACT_APP_kEY}`
+            );
+            setnews(response.data.articles);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     useEffect(() => {
-        fetch();
+        fetchData();
         setcateg(cat);
-    }, [cat])
+    }, [cat, fetchData, setcateg]);
 
-    const [categ, setcateg] = useState();
-
-
-    return (<>
-        <div className="container my-3"><h1>Top HEADLINES - <span style={{ textTransform: "capitalize" }}>{categ} </span></h1>
-            <div className="container d-flex my-3 justify-content-center align-items-center flex-column " style={{ minHeight: "100vh" }}>{news ? news.map((items, index) => <>
-
-                <div className="container my-3 p-3" style={{ width: "100%", boxShadow: "2px 1px 10px green" }}>
-
-                    <div className="container d-flex flex-row ">
-                        <div className="container d-flex my-5 ">
-                            <h5 className="">{items.title}</h5>
-                        </div>
-                        <div className="container">
-                            <img src={items.urlToImage} alt="/" className="img-fluid" style={{
-                                width
-                                    : "auto%", height: "300px", objectFit: "cover"
-                            }}></img>
-                        </div>
-
-                    </div>
-                    <p>{items.content}
-                        <a href={items.url} target="blank"> Read More</a></p>
+    return (
+        <>
+            <div className="container my-3">
+                <h1>Top HEADLINES - <span style={{ textTransform: "capitalize" }}>{categ}</span></h1>
+                <div className="container d-flex my-3 justify-content-center align-items-center flex-column " style={{ minHeight: "100vh" }}>
+                    {news.length > 0 ? (
+                        news.map((item, index) => (
+                            <div className="container my-3 p-3" style={{ width: "100%", boxShadow: "2px 1px 10px green" }} key={index}>
+                                <div className="container d-flex flex-row">
+                                    <div className="container d-flex my-5">
+                                        <h5>{item.title}</h5>
+                                    </div>
+                                    <div className="container">
+                                        <img src={item.urlToImage} alt="/" className="img-fluid" style={{ width: "auto%", height: "300px", objectFit: "cover" }} />
+                                    </div>
+                                </div>
+                                <p>{item.content} <a href={item.url} target="blank">Read More</a></p>
+                            </div>
+                        ))
+                    ) : (
+                        "Loading...."
+                    )}
                 </div>
-
-
-            </>
-
-            ) : "Loading...."}</div>
-        </div>
-
-    </>
+            </div>
+        </>
     );
 };
+
 export default FetchApi;
